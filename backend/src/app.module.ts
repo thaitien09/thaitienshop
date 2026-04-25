b1c84c80df1409a7ccb843a66c6f4a0fd370701e
Duong Thai Tien <vietnammonster2003@gmail.com>
2026-04-25 20:51:23 +0700

    test: disable rate limit

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -37,10 +37,10 @@ import { APP_GUARD } from '@nestjs/core';
   ],
   controllers: [],
   providers: [
-    {
-      provide: APP_GUARD,
-      useClass: ThrottlerGuard,
-    },
+    // {
+    //   provide: APP_GUARD,
+    //   useClass: ThrottlerGuard,
+    // },
   ],
 })
 export class AppModule { }
